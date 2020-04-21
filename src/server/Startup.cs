f12637b23e4384e2f95315a6f29cd31c3b2613c4
aakim-git <Aaron Kim>
2020-04-21 00:49:39 -0700

    Init

---
+++ src/server/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using CommentSection.Hubs;
+using System.IO;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
+namespace CommentSection
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+            services.AddSpaStaticFiles(Configuration =>
+            {
+                Configuration.RootPath = "../src/client/comment-section-client";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            else {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts(); // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = Path.Join(env.ContentRootPath, "/src/client/comment-section-client");
+                spa.UseReactDevelopmentServer(npmScript: "start");
+            });
+            
+            app.UseEndpoints(endpoints => {
+                endpoints.MapHub<ChatHub>("chatHub");
+            });
+        }
+    }
+}
